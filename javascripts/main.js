e4d9cba02a57b6d08ee74322c6572c779a3839f7
Lonkal <kalon.cheong@gmail.com>
2014-05-16 02:06:31 -0400

    jQuery popup effect and some href fixes

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,9 @@
-console.log('This would be the main JS file.');
+$(document).ready(function (){
+  $(".btn").hover(
+    function () {
+      $(this).css("font-size", "150%");
+    },
+    function(){
+      $(this).css("font-size", "100%")
+    });
+});
